d4f1d622b93bb37b43900f9a41501ed22b30dd48
ando <ando@jimbodenki.co.jp>
2020-11-28 22:05:44 +0900

    Add css proxy method

--- volume/apache2/html/index.php
+++ volume/apache2/html/index.php
@@ -4,18 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="/assets/css/bulma.min.css">
 </head>
 <body>
-<h1>cat2 container</h1>
-<div>
-    <dl>
-        <dt>http x forwarded for</dt>
-        <dd><?php echo $_SERVER['HTTP_X_FORWARDED_FOR']; ?></dd>
-        <dt>remote ip addr</dt>
-        <dd><?php echo $_SERVER['REMOTE_ADDR']; ?></dd>
-        <dt>server addr</dt>
-        <dd><?php echo $_SERVER['SERVER_ADDR']; ?></dd>
-    </dl>
+<div class="container">
+    <section>
+        <h1 class="title has-text-primary">cat2 container</h1>
+        <div>
+            <dl>
+                <dt>http x forwarded for</dt>
+                <dd><?php echo $_SERVER['HTTP_X_FORWARDED_FOR']; ?></dd>
+                <dt>remote ip addr</dt>
+                <dd><?php echo $_SERVER['REMOTE_ADDR']; ?></dd>
+                <dt>server addr</dt>
+                <dd><?php echo $_SERVER['SERVER_ADDR']; ?></dd>
+            </dl>
+        </div>
+    </section>
 </div>
 </body>
 </html>
